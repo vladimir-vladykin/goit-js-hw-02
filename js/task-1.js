8e9d866e12cf8807fd1221c882e7838c293d5fc3
Volodymyr Vladykin <vova@pocketguard.com>
2025-01-13 21:32:51 +0200

    task 1 complete

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,17 @@
-function makeTransaction(quantity, pricePerDroid) {
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = pricePerDroid * quantity;
+  const canCustomerAffordTransaction = customerCredits >= totalPrice;
+
+  if (!canCustomerAffordTransaction) {
+    return "Insufficient funds!";
+  }
+
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
 console.log("----------------"); // to have nice padding between tasks output in console
