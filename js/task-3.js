252a31f548b1ea2961d4982b3f3895d35687a08c
Volodymyr Vladykin <vova@pocketguard.com>
2025-01-13 21:38:47 +0200

    task 3 complete

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,13 @@
-function getElementWidth(content, padding, border) {
-  // assumes that we have border-box sizing enabled
-  return (
-    Number.parseFloat(content) +
-    Number.parseFloat(padding) * 2 +
-    Number.parseFloat(border) * 2
-  );
+function checkForSpam(message) {
+  const resolvedMessage = message.toLowerCase();
+  return resolvedMessage.includes("spam") || resolvedMessage.includes("sale");
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 console.log("----------------"); // to have nice padding between tasks output in console
